19058c3085c6cf984746d6e0d36f99c5f86f5286
kosu <kosumon27@gmail.com>
2022-04-27 15:40:39 +0900

    ✨ Literal Types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import type { NextPage } from "next";
 
-const foo: boolean = true;
-const bar: string = "aaa";
-const baz: number = 123;
-const hoge: null = null;
-const war: undefined = undefined;
+const boo: false = false;
+const str: "bar" = "bar";
+const num: 1 = 1;
+
+function double(x: 5): number {
+  return x * 2;
+}
+
+double(5);
 
 const Home: NextPage = () => {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+};
+
+const Component = (props: { foo?: true }) => {
+  if (props.foo) {
+    return <div>a</div>;
+  }
   return <div>test</div>;
 };
 
