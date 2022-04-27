2e2c63fff713d51db59d2dfcbdfae6c2e3aaeafc
kosu <kosumon27@gmail.com>
2022-04-27 15:52:50 +0900

    ✨ Widening

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,9 @@
 import type { NextPage } from "next";
 
-const boo: false = false;
-const str: "bar" = "bar";
-const num: 1 = 1;
-
-function double(x: 5): number {
-  return x * 2;
-}
-
-double(5);
+const boo = "boo" as const;
+let bar = boo;
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Component />
-    </div>
-  );
-};
-
-const Component = (props: { foo?: true }) => {
-  if (props.foo) {
-    return <div>a</div>;
-  }
   return <div>test</div>;
 };
 
