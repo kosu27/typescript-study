3a20b48c8e854ba470d3954f3d3bb98cdd69e9d4
kosu <kosumon27@gmail.com>
2022-04-28 15:28:49 +0900

    ✨ Array型

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
-const boo = "boo" as const;
-let bar = boo;
+const Arr: (number | string | boolean)[] = [1, "2", 3, true];
+const arr: Array<number> = [1, 2, 3];
 
 const Home: NextPage = () => {
   return <div>test</div>;
