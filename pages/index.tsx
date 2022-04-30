52b956b04fc4fc6ecc4d6c9b3d513577ed8257c1
kosu <kosumon27@gmail.com>
2022-04-30 13:22:37 +0900

    ✨ Tuple型

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
+//Array型
 const Arr: (number | string | boolean)[] = [1, "2", 3, true];
 const arr: Array<number> = [1, 2, 3];
+
+//Tuple型
+const tuple: [number, string] = [1, "foo"];
+tuple[0].toFixed(2);
 
 const Home: NextPage = () => {
   return <div>test</div>;
