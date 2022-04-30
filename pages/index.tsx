ba568e6cf986c4bc9ffcbf491767310d48206874
kosu <kosumon27@gmail.com>
2022-05-01 04:12:23 +0900

    ✨ オブジェクト

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,16 @@
 import type { NextPage } from "next";
 
-//Array型
-const Arr: (number | string | boolean)[] = [1, "2", 3, true];
-const arr: Array<number> = [1, 2, 3];
-
-//Tuple型
-const tuple: [number, string] = [1, "foo"];
-tuple[0].toFixed(2);
-
-//Any型
-const any: any = "何でも代入できる";
-
-//Unknown型
-const unknown: unknown = "何でも代入できる";
-
-if (typeof unknown === "string") {
-  unknown.substr(2);
-}
-
-//Void型
-const foo = (): void => {
-  alert("foo");
+//標準ライブラリ
+let obj3: Record<string, unknown> = {
+  a: 1,
+  b: "foo",
 };
-// function foo(): void {
-//   alert("Hello");
-//   return;
-// }
 
-//Never型
-const error = (): never => {
-  throw new Error("エラー");
+//Index Signature
+let obj4: { a: number; b: string; foo: string } = {
+  a: 1,
+  b: "foo",
+  foo: "bar",
 };
 
 const Home: NextPage = () => {
