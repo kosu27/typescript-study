541f906c2a00db1661e22ee4762e036672d12139
kosu <kosumon27@gmail.com>
2022-05-03 14:21:51 +0900

    ✨ Union Type

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,15 @@ type Foo = {
   a: number;
   b: string;
 };
-
 type Bar = {
   c: boolean;
 };
+type FooBar = Foo | Bar;
 
-type FooBar = Foo & Bar;
-
-const Test: FooBar = {
+const test: FooBar = {
   a: 1,
   b: "",
-  c: true,
 };
-//プリミティブ型として使う方法
-type Boo = string;
-type Baz = number;
-type BooBaz = Boo & Baz;
 
 const Home: NextPage = () => {
   return <div>test</div>;
