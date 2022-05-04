25bc8893d57bfac79303731a3e20fad43f2049f1
kosu <kosumon27@gmail.com>
2022-05-05 01:17:53 +0900

    ✨ in演算子を使ってUnion Typesの絞り込み

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,20 @@ type Foo = {
   b: string;
 };
 type Bar = {
+  a: string;
   c: boolean;
 };
 type FooBar = Foo | Bar;
 
 const test: FooBar = {
-  a: 1,
+  a: "1",
   b: "",
+  c: true,
 };
+
+if ("b" in test) {
+  test.a.toFixed();
+}
 
 const Home: NextPage = () => {
   return <div>test</div>;
