e2655f754145a7a11431200923d1a84ca539546b
kosu <kosumon27@gmail.com>
2022-05-08 14:50:35 +0900

    ✨ key of

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
-export function double(x: number | string) {
-  if (typeof x === "string") {
-    return Number(x) * 2;
-  }
-  return x * 2;
+export const Obj = {
+  foo: "foo",
+  bar: "bar",
+};
+
+// type Obj = typeof Obj;
+// type Key = keyof Obj;
+
+type Key = keyof typeof Obj;
+
+// const key: Key = "foo";
+
+function test(x: keyof typeof Obj) {
+  return;
 }
+
+test("foo");
