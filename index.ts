c429d004f48efe5ed371fe5ffa415fbb560f9dc6
kosu <kosumon27@gmail.com>
2022-05-10 01:59:23 +0900

    ✨in演算子を使ったType Guard

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-export const foo = (value?: string) => {
-  if (!value) {
-    return;
+type UserA = { name: string };
+type UserB = { name: string; nickName: string };
+
+export const foo = (value: UserA | UserB) => {
+  if ("nickName" in value) {
+    return value;
   }
   return value;
 };
