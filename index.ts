563bd87a3e861f2fac423c5d4818a8c1c26bffa0
kosu <kosumon27@gmail.com>
2022-05-10 02:08:49 +0900

    ✨ タグ付きUnion Typesを使ったType Guard

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
-type UserA = { name: string };
-type UserB = { name: string; nickName: string };
+type UserA = { name: string; lang: "ja" };
+type UserB = { name: string; lang: "en" };
+type UserC = { name: string; lang: "fr" };
 
-export const foo = (value: UserA | UserB) => {
-  if ("nickName" in value) {
-    return value;
+export const foo = (value: UserA | UserB | UserC) => {
+  switch (value.lang) {
+    case "ja": {
+      return value;
+    }
+    case "en": {
+      return value;
+    }
+    case "fr": {
+      return value;
+    }
+    default: {
+      throw Error("lang is not defined");
+      return value;
+    }
   }
-  return value;
+  //   if (value.lang === "ja") {
+  //     return value;
+  //   }
+  //   return value;
 };
