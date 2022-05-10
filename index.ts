92dc074f142b921da8163420cec4996ea200acce
kosu <kosumon27@gmail.com>
2022-05-11 00:56:56 +0900

    ✨ 関数のGenericsのextendsによる型制約

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-const foo = <T>(arg: T) => {
-  return { value: arg };
+const foo = <T extends string | number>(arg: T) => {
+  if (typeof arg === "string") {
+    return { value: arg.toUpperCase() };
+  }
+
+  return { value: arg.toFixed };
 };
 
-const foo1 = foo<string | null>("");
-const foo2 = foo(1);
-const foo3 = foo(true);
+// const foo1 = foo<string>("");
+// const foo2 = foo(1);
+// const foo3 = foo([true]);
