fce567824b4f24d8894c18a24e1241227af1f592
kosu <kosumon27@gmail.com>
2022-05-10 23:25:35 +0900

    ✨ Generics

--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
-type UserA = { name: string; lang: "ja" };
-type UserB = { name: string; lang: "en" };
-
-const isUserA = (user: UserA | UserB): user is UserA => {
-  return user.lang === "ja";
+export type User<T> = {
+  name: string;
+  state: T;
 };
 
-const isUserB = (user: UserA | UserB): user is UserB => {
-  return user.lang === "en";
+type Japanese = User<"東京都" | "大阪府">;
+type American = User<"New York" | "Los Angeles">;
+
+const user1: Japanese = {
+  name: "田中",
+  state: "東京都",
 };
 
-const users: (UserA | UserB)[] = [
-  { name: "田中", lang: "ja" },
-  { name: "山田", lang: "ja" },
-  { name: "ジョニー", lang: "en" },
-];
-
-const japanese = users.filter(isUserA);
-const notJapanese = users.filter(isUserB);
+const user2: American = {
+  name: "John",
+  state: "New York",
+};
