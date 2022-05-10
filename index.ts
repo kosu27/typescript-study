d9d239309f1cdeabc4bdd24a7948144ea21bf701
kosu <kosumon27@gmail.com>
2022-05-11 00:40:29 +0900

    ✨ 暗黙的な型解決

--- index.ts
+++ index.ts
@@ -2,4 +2,6 @@ const foo = <T>(arg: T) => {
   return { value: arg };
 };
 
-const foo1 = foo<{ foo: number[] }>({ foo: [1, 2, 3] });
+const foo1 = foo<string | null>("");
+const foo2 = foo(1);
+const foo3 = foo(true);
