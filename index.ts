8f659e2e8940fad9592db2229e44eafa12325542
kosu <kosumon27@gmail.com>
2022-05-11 01:55:02 +0900

    ✨ JSメソッドで用いられるGenerics

--- index.ts
+++ index.ts
@@ -1,17 +1,1 @@
-const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
-  return obj[key];
-};
-
-const setProperty = <T, K extends keyof T>(obj: T, key: K, value: T[K]) => {
-  obj[key] = value;
-};
-
-const obj = {
-  foo: 1,
-  bar: 2,
-  baz: 3,
-};
-
-const hoge = getProperty(obj, "baz");
-
-setProperty(obj, "bar", 5);
+const foo = [1, 2, 3].map((v) => v);
