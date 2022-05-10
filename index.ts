23d0b4c8ab64633cf37fae5f81694d8320347e2d
kosu <kosumon27@gmail.com>
2022-05-10 23:28:51 +0900

    ✨ Genericsの初期値

--- index.ts
+++ index.ts
@@ -1,17 +1,10 @@
-export type User<T> = {
-  name: string;
-  state: T;
+export type Foo<T = string> = {
+  value: T;
 };
 
-type Japanese = User<"東京都" | "大阪府">;
-type American = User<"New York" | "Los Angeles">;
-
-const user1: Japanese = {
-  name: "田中",
-  state: "東京都",
+const foo1: Foo = {
+  value: "",
 };
-
-const user2: American = {
-  name: "John",
-  state: "New York",
+const foo2: Foo<number> = {
+  value: 1,
 };
