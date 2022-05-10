4b7bac7e1a2f7aeef9e720f7667993087b22696a
kosu <kosumon27@gmail.com>
2022-05-11 01:16:09 +0900

    ✨ Genericsの型引数が複数あるパターン

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
-const foo = <T extends string | number>(arg: T) => {
-  if (typeof arg === "string") {
-    return { value: arg.toUpperCase() };
-  }
-
-  return { value: arg.toFixed };
+const foo = <T extends string, K extends number, U = boolean>(
+  foo: T,
+  bar: K,
+  baz: U
+) => {
+  return {};
 };
-
-// const foo1 = foo<string>("");
-// const foo2 = foo(1);
-// const foo3 = foo([true]);
