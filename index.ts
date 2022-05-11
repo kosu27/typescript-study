843b03d198795c6f9bc2d5640a47115f76e7bdd1
kosu <kosumon27@gmail.com>
2022-05-11 18:54:20 +0900

    ✨ Record

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-type Foo = NonNullable<string | null | undefined>;
+type Foo = Record<"hoge" | "fuga", 1 | 3>;
+
+const obj: Foo = {
+  hoge: 1,
+  fuga: 3,
+};
 
 // type User = {
 //   name: string;
