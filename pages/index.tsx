f230145a9b325df2f6211ed640ebf4c03b0e89bb
kosu <kosumon27@gmail.com>
2022-05-12 02:06:12 +0900

    ⚡️ リストのロジック部分

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,25 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [todos, setTodos] = useState([
+    { id: Math.random(), label: "TODO1", isDone: false },
+    { id: Math.random(), label: "TODO2", isDone: true },
+    { id: Math.random(), label: "TODO3", isDone: true },
+    { id: Math.random(), label: "TODO4", isDone: false },
+  ]);
+
+  const toggle = (e) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === Number(e.target.value)) {
+          return { ...todo, isDone: !todo.isDone };
+        }
+        return todo;
+      });
+    });
+  };
+
   return (
     <div className="w-96 mx-auto p-20">
       <h1 className="text-xl font-bold">TODO</h1>
@@ -8,25 +27,20 @@ const Home: NextPage = () => {
         <input type="text" className="border border-black" />
         <button className="border border-black shrink-0 px-2">追加</button>
       </div>
-      <ul className="mt-4 spaxe-y-2">
-        <li>
-          <label className="flex items-center gap-x-2">
-            <input type="checkbox" />
-            <span>TODO1</span>
-          </label>
-        </li>
-        <li>
-          <label className="flex items-center gap-x-2">
-            <input type="checkbox" />
-            <span>TODO2</span>
-          </label>
-        </li>
-        <li>
-          <label className="flex items-center gap-x-2">
-            <input type="checkbox" />
-            <span>TODO3</span>
-          </label>
-        </li>
+      <ul className="mt-4 space-y-2">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label className="flex items-center gap-x-2">
+              <input
+                type="checkbox"
+                value={todo.id}
+                checked={todo.isDone}
+                onChange={toggle}
+              />
+              <span>{todo.label}</span>
+            </label>
+          </li>
+        ))}
       </ul>
     </div>
   );
